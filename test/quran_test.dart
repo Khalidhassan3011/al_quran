dddbf0a6e18a8e3ff5be56a13113775e5b1097fc
Khalidhassan3011 <khalidhassan3022@gmail.com>
2022-09-01 11:57:23 +0600

    add test

--- test/quran_test.dart
+++ test/quran_test.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'package:al_quran/al_quran.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  /// test constant value (count)
+  test("Test constant value", () {
+    // number of surah
+    expect(114, AlQuran.quranDetails.count.surah);
+
+    // number of surah for all surah
+    expect(114, AlQuran.quranDetails.getFullQuran.length);
+  });
+}
